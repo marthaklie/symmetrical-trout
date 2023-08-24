67a96d6db6405328cd17a7107e4638215b8607f7
marthaklie <138883071+marthaklie@users.noreply.github.com>
2023-08-24 13:48:08 +0000

    Pending changes exported from your codespace

--- src/app.js
+++ src/app.js
@@ -9,3 +9,27 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+// Función para generar una excusa
+function generadorDeExcusa() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let indexWho = [Math.floor(Math.random() * who.length)];
+  return excusaAleatoria;
+  let indexAcmtion = Math.floor(Math.random() * action.length);
+  let indexWhat = Math.floor(Math.random() * what.length);
+  let indexWhen = Math.floor(Math.random() * when.length);
+
+  // Asignar la excusa generada al elemento con el ID "excuse"
+  botonGenerarExcusa.addEventListener("click", function() {
+    var excusaGenerada = generadorDeExcusa();
+    elementoExcusa.innerHTML = excusaGenerada;
+  });
+}
